a6b2e17d414edff77fef7aa68e4352924362f2b0
Natsoulas <nnatsoulas@gmail.com>
2024-09-19 13:52:29 -0600

    seguimos laborando

--- orbit-visualization.js
+++ orbit-visualization.js
@@ -1,5 +1,5 @@
-import * as THREE from 'https://unpkg.com/three@0.134.0/build/three.module.js';
-import { OrbitControls } from 'https://unpkg.com/three@0.134.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
+import { OrbitControls } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/examples/jsm/controls/OrbitControls.js';
 
 let scene, camera, renderer, controls;
 let orbit, centralBody, satellite;
