0884163e1b9db2ccc06799988e6fd665d9739af7
Natsoulas <nnatsoulas@gmail.com>
2024-09-19 13:56:03 -0600

    console log for debug

--- orbit-visualization.js
+++ orbit-visualization.js
@@ -1,3 +1,5 @@
+console.log("Orbit visualization script loaded");
+
 import * as THREE from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.module.js';
 import { OrbitControls } from 'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/examples/jsm/controls/OrbitControls.js';
 
